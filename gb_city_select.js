fc0bb0046ca3d010485af973f15202b08bc6911d
Nikita Malyshev <Niklan@1450656.no-reply.drupal.org>
2012-12-15 16:47:21 +0600

    - Added README.txt

--- gb_city_select.js
+++ gb_city_select.js
@@ -15,7 +15,7 @@
         }
 
         function getCookie (cookie_name) {
-            var results = document.cookie.match ( '(^|;) ?' + cookie_name + '=([^;]*)(;|$)' );
+            var results = document.cookie.match ('(^|;) ?' + cookie_name + '=([^;]*)(;|$)');
             if (results) {
                 return (unescape(results[2]));
             } else {
