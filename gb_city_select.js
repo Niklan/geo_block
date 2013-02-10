43aad3271ab57b4d3f0909cc1096a0074cc0b7a0
Niklan <Niklan@1450656.no-reply.drupal.org>
2013-02-10 18:58:32 +0600

    Fixes #1867346.

--- gb_city_select.js
+++ gb_city_select.js
@@ -5,8 +5,8 @@
  * gb.module and list based on the established units and cities for them.
  *
  */
-(function ($, Drupal, window, document, undefined) {
-  Drupal.behaviors.exampleModule = {
+(function ($) {
+  Drupal.behaviors.gb = {
     attach: function (context, settings) {
       function setCookie(c_name,value,exdays) {
         var exdate = new Date();
@@ -35,14 +35,14 @@
       var user_city = Drupal.settings.user_city;
       var text = Drupal.settings.text;
 
-      /**
+     /**
      * This is to ensure that after the selection of the city did not write Object.
      */
       if ($.isPlainObject(user_city)) {
         user_city = user_city[0];
       }
 
-      /**
+     /**
      * Configurations for "TOP TOOLBAR"
      */
       if (Drupal.settings.gb_top_toolbar_enabled == 1) {
@@ -81,7 +81,6 @@
             'margin':'15px auto',
             'font-size':'18px'
           });
-
 
           $('#gb_city_select_1_close').css({
             'background':'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAAkCAYAAACE7WrnAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAmZJREFUeNqtlMFrE0EUxk0CzUGhUCgILRahgifpreQQKpaC4E08VRQPhWLw2kLooVRqUhQ9GhClUNGLNxEUCgEh2D9AzMWCUPRSaKBQCBRK/D2YgW+zO8129cG3b/bNvG/ezO77cr1e71y/1ev1IdwcuAEuuHAHfKlWq1/xMfNESjKD2wSXQZJ9B3chxEeJtIrn4BEYZMfgMWRPfCAvk6+FZJANgXU2X4kQEZjH3ZPSZ8GbBIJXbs4fa43cKa1oTRa/oOQmWGD8TOKrxBZtTuIFn1soFosl/JIk5Fut1odyudwD24zPE/sMwbqrvmAOTAKzS6xp5Gq12kNeXgK19+A+yScJv8U7cAeozdrRLoJ+mwdbbvcQidqoER2G/hetiPExrh1Ye2REexLQi93wdyJkqzjiMftldzTCYB/4hAYJlb7jHIEFXyHxLfld9ohP5HlYD70V9p2EO3kANqW6pqxv+BaxpDHGP8Aw+OMmb4ESUGuBbdcBo2AXXKOYrvbaHO4ToJJU1gFlSNqRXiNgO113FQ0yS572JErkyXZwV8Fy4FPbfMUdZzesR3GBG5ZWaNtd6PxAos74laBCjvz+mU4hIUmlkBAmKyQEZ1ZIyP6PQrJ5VCEJZFZIcsMKSclNEFNIYosgUSFzB2OTJfw3SfgIbpNw4qp9alLj74P3guuAm8CsC8aNKKiQjky/aDaFtN2FJLtCakWMsykkiRv+ToQsrJDcUUwhSag4kohCygeIKCTxiTyPmEIqiSoksdMVkgX/pJAU09Vey6SQkEQVksCZFdKTKJEnS62QrE2vkBw3opAkBxXyL4igTk91D97/AAAAAElFTkSuQmCC")',
@@ -254,4 +253,4 @@
       }
     }
   }
-})(jQuery, Drupal, this, this.document);
+})(jQuery);
